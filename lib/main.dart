2fed1d4b9a17c454b375935a0a597e17f3651814
Krísyorwing Mata <krismata12@gmail.com>
2023-08-10 23:40:38 -0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:llm_trivia/firebase_options.dart';
 import 'package:llm_trivia/home_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
